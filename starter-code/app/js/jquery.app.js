a5a872139488d8bb22b013006c2ca73c490abb5f
Gisellabella <gisella@gmail.com>
2016-11-30 20:07:31 -0700

    fixed the posting issue. All set.

--- starter-code/app/js/jquery.app.js
+++ starter-code/app/js/jquery.app.js
@@ -4,49 +4,37 @@
 	var catList = $.get("https://ga-cat-rescue.herokuapp.com/api/cats").done(function(catList){
 	    var catListParsed = jQuery.parseJSON(catList);
 	    var fullCatList = [];
-	    
-	    for (var  i = 0; i < catListParsed.length; i++) {
+	   	for (var  i = 0; i < catListParsed.length; i++) {
 	        fullCatList.push(catListParsed[i].name);
 	    }
-			var catNode = document.getElementById("cats");
+		var catNode = document.getElementById("cats");
 			
-			function write_catList(){
+		function write_catList(){
 			    for (i = 0; i < fullCatList.length; i++ ) {
-			    	var li = document.createElement("li");
-			    	var t = document.createTextNode(fullCatList[i]);
-			    	li.appendChild(t); 
-			    	catNode.appendChild(li);
+			    var li = document.createElement("li");
+			    var t = document.createTextNode(fullCatList[i]);
+			    li.appendChild(t); 
+			    catNode.appendChild(li);
 			    	//console.log(catNode);
 				}
 			}
 		
-	write_catList();
+		write_catList();
 
-$( "#new-cat" ).submit(function( event ) {
-  alert( "Handler for .submit() called." );
-  event.preventDefault();
-});	
+	$( "#new-cat" ).submit(function( event ) {
+  		//alert( "Handler for .submit() called." );
+  		event.preventDefault();
+		 var $catName = $('#cat-name').val();//storing inputs
+		 var $catNote = $('#cat-note').val();
+		 $.ajax ({
+				    type: "POST",
+				    url: "https://ga-cat-rescue.herokuapp.com/api/cats",
+				    data: JSON.stringify({ 'name' : $catName, 'note' : $catNote}),//pasing the values into a string
+				    dataType: "json"
+				    }).done(function(newKitty) {
+				       console.log(newKitty);
+				       });
 
-var newCat={//creating new object
- name: $('#cat-name').val(),
- note: $('#cat-note').val(),
-};
+			    });
+  });
 
-$.ajax({
-        type: "POST",
-        url: "https://ga-cat-rescue.herokuapp.com/api/cats",
-        data: JSON.stringify
-
-    });
-
-
-
-
-
-
-
-
-
-
-  
-});
